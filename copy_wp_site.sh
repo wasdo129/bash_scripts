d4511a2137a9044902551cce876ff002b377be33
wasdo129 <109848340+wasdo129@users.noreply.github.com>
2023-05-17 15:18:32 +0300

    Update copy_wp_site.sh

--- copy_wp_site.sh
+++ copy_wp_site.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 
-read -p "Введите имя директории: " name_dir
+read -p "Введите имя старой директории: " old_name_dir
+read -p "Введите имя новой директории: " new_name_dir
 read -p "Введите имя старой бд: " old_bd_name
 read -p "Введите имя новой бд: " new_bd_name
 read -p "Введите пароль базы данных: " bd_pass
-DT=`date '+%Y%m%d'`
-
-cp -r ~/wordpress_617xh ~/wordpress_617xh.copy
-
-sed -i 's/'$old_bd_name'/'$new_bd_name'/g' ~/$name_dir.copy/public_html/wp-config.php
-
 read -p "Введите имя старого домена: " old_url_name
 read -p "Введите имя нового домена: " new_url_name
+DT=`date '+%Y%m%d'`
+
+if [ "$old_name_dir" == "public_html" ]; then
+  mkdir $new_name_dir && cp -r ~/public_html ~/$new_name_dir
+else
+  cp -r ~/$old_name_dir ~/$new_name_dir
+fi
+
+sed -i 's/'$old_bd_name'/'$new_bd_name'/g' ~/$new_name_dir/public_html/wp-config.php
 
 mysqldump -u''$old_bd_name'' $old_bd_name -p''$bd_pass'' > $old_bd_name.$DT.sql --no-tablespaces
 mysql -u''$new_bd_name'' $new_bd_name -p''$bd_pass'' < $old_bd_name.$DT.sql
 
-cd ~/$name_dir.copy/public_html && wp search-replace $old_url_name $new_url_name --all-tables --report-changed-only
+cd ~/$new_name_dir/public_html && wp search-replace $old_url_name $new_url_name --all-tables --report-changed-only
 
-find ~/$name_dir.copy -type f -name '*' -exec sed -i -r 's/'$old_url_name'/'$new_url_name'/g' {} \;
+find ~/$new_name_dir -type f -name '*' -exec sed -i -r 's/'$old_url_name'/'$new_url_name'/g' {} \;
